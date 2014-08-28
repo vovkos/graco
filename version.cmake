b4aced0ba773e5e66725824b818947d81ebecab3
Vladimir Gladkov <vovkos@gmail.com>
2014-08-28 15:26:44 +0800

    [ioninja] release 3.1.1 beta

--- version.cmake
+++ version.cmake
@@ -1,7 +1,7 @@
 set (VERSION_MAJOR     1)
 set (VERSION_MINOR     0)
 set (VERSION_REVISION  0)
-set (VERSION_TAG       beta)
+set (VERSION_TAG)
 
 if (NOT "${VERSION_TAG}" STREQUAL "")
 	set (VERSION_TAG_SUFFIX " ${VERSION_TAG}")

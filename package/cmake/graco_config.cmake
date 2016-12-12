b2309dce916b2b021d3adb8df40531e502ddf840
Vladimir Gladkov <vovkos@gmail.com>
2016-12-12 18:03:01 +0800

    [graco_package] create both binary- and source- zip packages (currently windows only); add license, test and package dirs to the source zip

--- package/cmake/graco_config.cmake
+++ package/cmake/graco_config.cmake
@@ -10,11 +10,13 @@
 #...............................................................................
 
 set (GRACO_ROOT_DIR   "${CMAKE_CURRENT_LIST_DIR}/..")
+set (GRACO_CMAKE_DIR  "${CMAKE_CURRENT_LIST_DIR}")
 set (GRACO_INC_DIR    "${GRACO_ROOT_DIR}/include")
 set (GRACO_FRAME_DIR  "${GRACO_ROOT_DIR}/frame")
 set (GRACO_SAMPLE_DIR "${GRACO_ROOT_DIR}/samples")
 set (GRACO_EXE        "${GRACO_ROOT_DIR}/bin/graco")
 
-include ("${GRACO_ROOT_DIR}/cmake/graco_step.cmake")
+include (version.cmake)
+include (graco_step.cmake)
 
 #...............................................................................

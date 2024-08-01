e4a08e33c281776eb194657cc4d3fa2bce54071d
Vladimir Gladkov <vovkos@gmail.com>
2024-08-01 23:24:23 +0300

    [ci] major update:
    - use actions/checkout@v4
    - run on macos-13
    - run on ubuntu-22.04
    - use wget for file downloads

--- ci/github-actions/install-windows.bat
+++ ci/github-actions/install-windows.bat
@@ -16,12 +16,20 @@ set DOWNLOAD_DIR_CMAKE=%DOWNLOAD_DIR:\=/%
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
+:: wget
+
+echo Installing wget...
+
+choco install wget --no-progress || exit
+
+:: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
 :: Ragel
 
-echo Downloading Ragel...
+echo Downloading %RAGEL_DOWNLOAD_URL%...
 
 mkdir %DOWNLOAD_DIR%
-powershell "Invoke-WebRequest -Uri %RAGEL_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\ragel.exe"
+wget -nv %RAGEL_DOWNLOAD_URL% -O %DOWNLOAD_DIR%\ragel.exe || exit
 
 echo set (RAGEL_EXE %DOWNLOAD_DIR_CMAKE%/ragel.exe) >> paths.cmake
 
@@ -29,11 +37,11 @@ echo set (RAGEL_EXE %DOWNLOAD_DIR_CMAKE%/ragel.exe) >> paths.cmake
 
 :: Lua (CMake-based)
 
-echo Downloading Lua...
+echo Downloading %LUA_DOWNLOAD_URL%...
 
 mkdir %DOWNLOAD_DIR%\lua
-powershell "Invoke-WebRequest -Uri %LUA_DOWNLOAD_URL% -OutFile %DOWNLOAD_DIR%\lua\lua.zip"
-7z x -y %DOWNLOAD_DIR%\lua\lua.zip
-ren Lua-%LUA_VERSION% lua
+wget -nv %LUA_DOWNLOAD_URL% -O %DOWNLOAD_DIR%\lua\lua.zip || exit
+7z x -y %DOWNLOAD_DIR%\lua\lua.zip || exit
+ren lua-%LUA_VERSION% lua || exit
 
 ::..............................................................................
